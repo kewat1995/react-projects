bda318ca01da9300da4ed2c01079cafc5c681cb0
w3schools-test <123186007+kewat1995@users.noreply.github.com>
2023-09-12 13:57:09 +0100

    my first react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navigation from './component/Navigation'
+import Herosection from './component/hero'
+const App = () =>{
+
+  return (
+    <div>
+      <Navigation/>
+      <Herosection/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/hero.jsx
@@ -0,0 +1,29 @@
+function Herosection() {
+  return (
+    <main className="hero container">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+        <div className="hero-btn">
+          <button>Shop now</button>
+          <button className="secondry-btn">Category</button>
+        </div>
+        <div className="shoping">
+          <p>Also Available On</p>
+          <div className="icon">
+            <img src="/images/flipkart.png" alt="flipcart" />
+            <img src="/images/amazon.png" alt="amazon" />
+          </div>
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src="/images/shoe_image.png" alt="flipcart" />
+      </div>
+    </main>
+  );
+}
+export default Herosection;
